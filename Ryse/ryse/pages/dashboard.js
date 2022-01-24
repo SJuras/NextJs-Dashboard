932a1435cec6376877b79e4e44ae9bda829c0c0b
SJuras <jurasstpko@gmailcom>
2022-01-24 07:20:25 +0100

    v1.2.0 cards markup done.

--- Ryse/ryse/pages/dashboard.js
+++ Ryse/ryse/pages/dashboard.js
@@ -41,6 +41,8 @@ export default function dashboard() {
 
   const [display, changeDisplay] = useState('hide');
 
+  const [value, changeValue] = useState(1);
+
   return(
     <Flex h="100vh" flexDir="row" overflow="hidden" maxW="2000px">
       {/* Column 1 */}
@@ -237,6 +239,114 @@ export default function dashboard() {
           </Flex>
         </Flex>
         <Heading letterSpacing="tight">My Cards</Heading>
+        {value == 1 &&
+                    <Box
+                        borderRadius="25px"
+                        mt={4}
+                        w="100%"
+                        h="200px"
+                        bgGradient="linear(to-t, #B57295, #29259A)"
+                    >
+                        <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
+                            <Flex justify="space-between" w="100%" align="flex-start">
+                                <Flex flexDir="column">
+                                    <Text color="gray.400">Current Balance</Text>
+                                    <Text fontWeight="bold" fontSize="xl">$5,750.10</Text>
+                                </Flex>
+                                <Flex align="center">
+                                    <Icon mr={2} as={FiCreditCard} />
+                                    <Text fontWeight="bold">Ryse.</Text>
+                                </Flex>
+                            </Flex>
+                            <Text mb={4}>**** **** **** 1289</Text>
+                            <Flex align="flex-end" justify="space-between">
+                                <Flex>
+                                    <Flex flexDir="column" mr={4}>
+                                        <Text textTransform="uppercase" fontSize="xs">Valid Thru</Text>
+                                        <Text fontSize="lg">12/23</Text>
+                                    </Flex>
+                                    <Flex flexDir="column">
+                                        <Text textTransform="uppercase" fontSize="xs">CVV</Text>
+                                        <Text fontSize="lg">***</Text>
+                                    </Flex>
+                                </Flex>
+                                <Icon as={FiCreditCard} />
+                            </Flex>
+                        </Flex>
+                    </Box>
+                }
+                {value == 2 &&
+                    <Box
+                        borderRadius="25px"
+                        mt={4}
+                        w="100%"
+                        h="200px"
+                        bgGradient="linear(to-t, yellow.300, blue.500)"
+                    >
+                        <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
+                            <Flex justify="space-between" w="100%" align="flex-start">
+                                <Flex flexDir="column">
+                                    <Text color="gray.400">Current Balance</Text>
+                                    <Text fontWeight="bold" fontSize="xl">$350.00</Text>
+                                </Flex>
+                                <Flex align="center">
+                                    <Icon mr={2} as={FiCreditCard} />
+                                    <Text>Rise.</Text>
+                                </Flex>
+                            </Flex>
+                            <Text mb={4}>**** **** **** 8956</Text>
+                            <Flex align="flex-end" justify="space-between">
+                                <Flex>
+                                    <Flex flexDir="column" mr={4}>
+                                        <Text textTransform="uppercase" fontSize="xs">Valid Thru</Text>
+                                        <Text fontSize="lg">9/24</Text>
+                                    </Flex>
+                                    <Flex flexDir="column">
+                                        <Text textTransform="uppercase" fontSize="xs">CVV</Text>
+                                        <Text fontSize="lg">***</Text>
+                                    </Flex>
+                                </Flex>
+                                <Icon as={FiCreditCard} />
+                            </Flex>
+                        </Flex>
+                    </Box>
+                }
+                {value == 3 &&
+                    <Box
+                        borderRadius="25px"
+                        mt={4}
+                        w="100%"
+                        h="200px"
+                        bgGradient="linear(to-t, orange.300, pink.600)"
+                    >
+                        <Flex p="1em" color="#fff" flexDir="column" h="100%" justify="space-between">
+                            <Flex justify="space-between" w="100%" align="flex-start">
+                                <Flex flexDir="column">
+                                    <Text color="gray.400">Current Balance</Text>
+                                    <Text fontWeight="bold" fontSize="xl">$2,150.72</Text>
+                                </Flex>
+                                <Flex align="center">
+                                    <Icon mr={2} as={FiCreditCard} />
+                                    <Text>Rise.</Text>
+                                </Flex>
+                            </Flex>
+                            <Text mb={4}>**** **** **** 8353</Text>
+                            <Flex align="flex-end" justify="space-between">
+                                <Flex>
+                                    <Flex flexDir="column" mr={4}>
+                                        <Text textTransform="uppercase" fontSize="xs">Valid Thru</Text>
+                                        <Text fontSize="lg">11/22</Text>
+                                    </Flex>
+                                    <Flex flexDir="column">
+                                        <Text textTransform="uppercase" fontSize="xs">CVV</Text>
+                                        <Text fontSize="lg">***</Text>
+                                    </Flex>
+                                </Flex>
+                                <Icon as={FiCreditCard} />
+                            </Flex>
+                        </Flex>
+                    </Box>
+                }
       </Flex>
     </Flex>
   );
